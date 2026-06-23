d748b829c9516c8c7d8409ca634e3d964261035b
GOWTHAM <gowthamv24am@srishakthi.ac.in>
2026-06-23 22:23:45 +0530

    Time: 18 ms (9.84%), Space: 19.2 MB (52.99%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0 or x%10==0 and x!=0:
+            return False
+        xcopy=x
+        reverse=0
+        while(x):
+            reverse=(reverse*10)+(x%10)
+            x//=10
+        return xcopy==reverse
+            
+
