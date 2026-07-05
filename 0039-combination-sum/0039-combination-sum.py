c13dc94d14a1a230891ad83e260c189738930635
GOWTHAM <gowthamv24am@srishakthi.ac.in>
2026-07-05 12:44:05 +0530

    Time: 10 ms (51.69%), Space: 19.5 MB (82.31%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res=[]
+        p=[]
+        def findt(i,s):
+            
+            if i>=len(candidates) or s>target:
+                return
+           
+            if s==target:
+    
+            
+                res.append(p.copy())
+                return
+            p.append(candidates[i])
+            findt(i,s+candidates[i])
+            p.pop()
+            findt(i+1,s)
+            return
+        findt(0,0)
+        return res
